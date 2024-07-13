200e71487afa0a46912ba1d8b105dd8091e49d29
Hélio Tarnowski <60065662+heliotarnowski@users.noreply.github.com>
2024-07-13 15:27:51 -0300

    Create config.js

---
+++ cp-webpage/js/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_021as5dd', // e.g. us-east-1_uXboG5pAb
+        userPoolClientId: '21sda32s1dasd512230as1d2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-1
+    }
+};
