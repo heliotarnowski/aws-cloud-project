5b8c692640d597e14e1cc0bc80d472358b0fe711
Hélio Tarnowski <60065662+heliotarnowski@users.noreply.github.com>
2024-07-13 18:02:08 -0300

    Adjusting files

--- cp-webpage/js/config.js
+++ cp-webpage/js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_021as5dd', // e.g. us-east-1_uXboG5pAb
-        userPoolClientId: '21sda32s1dasd512230as1d2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_04U6FpR7i', // e.g. us-east-1_uXboG5pAb
+        userPoolClientId: '4kuttdsv35rlhqrrvigqpj2ha7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-1
     }
 };
---
+++ cp-webpage/js/session.js
@@ -0,0 +1,16 @@
+var Project = window.Project || {};
+
+(function SessionScopeWrapper($) {
+    var authToken;
+    Project.authToken.then(function setAuthToken(token) {
+        if (token) {
+            authToken = token;
+        } else {
+            window.location.href = '/index.html';
+        }
+    }).catch(function handleTokenError(error) {
+        alert(error);
+        window.location.href = '/index.html';
+    });
+
+}(jQuery));
