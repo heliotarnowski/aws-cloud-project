4bdbfadd3859c4082df35ceddde3f7ca029b28a5
Hélio Tarnowski <60065662+heliotarnowski@users.noreply.github.com>
2024-07-13 18:02:48 -0300

    Update config.js

--- cp-webpage/js/config.js
+++ cp-webpage/js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_04U6FpR7i', // e.g. us-east-1_uXboG5pAb
-        userPoolClientId: '4kuttdsv35rlhqrrvigqpj2ha7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_01231sdf', // e.g. us-east-1_uXboG5pAb
+        userPoolClientId: '6s5d4f65sf1sdfsdfdsffsdf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-1
     }
 };
